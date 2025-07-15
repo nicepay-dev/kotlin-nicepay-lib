e1fe517ea62f57058754b0f8213ef9b8447f35b2
Inka Fazaarillah <fazarillah@gmail.com>
2025-07-15 13:25:02 +0700

    chore: update build.gradle.kts and settings.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,16 @@
 plugins {
 	kotlin("jvm") version "1.9.25"
-	`maven-publish`
-	signing
-	id("org.jetbrains.dokka") version "1.9.10"
+	id("maven-publish")
+	id("signing")
 }
 
 group = "io.github.nicepay-dev"
 version = "1.0.0"
+val artifactId = "nicepay-kotlin-client"
 
 java {
+	withSourcesJar()
+	withJavadocJar()
 	toolchain {
 		languageVersion.set(JavaLanguageVersion.of(17))
 	}
@@ -19,47 +21,37 @@ repositories {
 }
 
 dependencies {
-	implementation("com.google.code.gson:gson:2.10.1")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("commons-codec:commons-codec:1.15")
+	implementation("com.google.code.gson:gson:2.10.1")
 	implementation("com.squareup.retrofit2:retrofit:2.9.0")
 	implementation("com.squareup.retrofit2:converter-jackson:2.9.0")
 	implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 	implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.17.2")
-
-	testImplementation(kotlin("test"))
+	implementation("org.slf4j:slf4j-api:2.0.13")
+	implementation("org.apache.logging.log4j:log4j-api:2.20.0")
+	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	implementation("ch.qos.logback:logback-classic:1.4.14")
 }
 
 tasks.test {
 	useJUnitPlatform()
 }
 
-// ✅ Register sourcesJar and javadocJar BEFORE using them in publishing
-val sourcesJar by tasks.registering(Jar::class) {
-	archiveClassifier.set("sources")
-	from(sourceSets["main"].allSource)
-}
-
-val javadocJar by tasks.registering(Jar::class) {
-	dependsOn("dokkaJavadoc")
-	archiveClassifier.set("javadoc")
-	from(tasks["dokkaJavadoc"])
-}
-
 publishing {
 	publications {
 		create<MavenPublication>("maven") {
-			from(components["java"])
-			artifact(sourcesJar.get())
-			artifact(javadocJar.get())
-
-			groupId = "io.github.nicepay-dev"
-			artifactId = "nicepay-kotlin-client"
+			groupId = project.group.toString()
+			artifactId = artifactId
 			version = project.version.toString()
 
+			from(components["java"])
+
 			pom {
-				name.set("kotlin-nicepay-lib")
-				description.set("NICEPay Kotlin Client Library")
+				name.set("nicepay-kotlin-client")
+				description.set("Official Kotlin client for NICEPAY Payment APIs (SNAP and V2)")
 				url.set("https://github.com/nicepay-dev/kotlin-nicepay-lib")
 
 				licenses {
@@ -94,7 +86,7 @@ publishing {
 
 				scm {
 					connection.set("scm:git:https://github.com/nicepay-dev/kotlin-nicepay-lib.git")
-					developerConnection.set("scm:git:ssh://git@github.com:nicepay-dev/kotlin-nicepay-lib.git")
+					developerConnection.set("scm:git:ssh://github.com:nicepay-dev/kotlin-nicepay-lib.git")
 					url.set("https://github.com/nicepay-dev/kotlin-nicepay-lib")
 				}
 			}
@@ -103,20 +95,16 @@ publishing {
 
 	repositories {
 		maven {
-			name = "sonatype"
+			name = "centralPortal"
 			url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
 			credentials {
-				username = findProperty("ossrhUsername") as String? ?: ""
-				password = findProperty("ossrhPassword") as String? ?: ""
+				username = findProperty("mavenCentralUsername") as String? ?: System.getenv("MAVEN_CENTRAL_USERNAME")
+				password = findProperty("mavenCentralPassword") as String? ?: System.getenv("MAVEN_CENTRAL_PASSWORD")
 			}
 		}
 	}
 }
 
 signing {
-	useInMemoryPgpKeys(
-		findProperty("signing.key") as String?,
-		findProperty("signing.password") as String?
-	)
 	sign(publishing.publications["maven"])
 }
